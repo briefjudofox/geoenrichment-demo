3dc09cd01bddbdb5dcebe541959bdf5c2d6285cf
Brief Fox <briefjudofox@gmail.com>
2013-10-08 15:56:13 -0700

    Added age donut chart for ring pop-ups using d3.

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,8 @@
 
-
 //init map
 var map;
 var gs;
+
 $(document).ready(function () {
 
   map = L.map('map', {
@@ -45,6 +45,7 @@ $(document).ready(function () {
   gs = new Geoservices.Geoservices();
 });
 
+//Geo Enrich the specified layer
 function enrich(type, layer){
     var enrichService = gs.GeoEnrichmentService({token:agoToken});
     var params;
@@ -56,31 +57,15 @@ function enrich(type, layer){
       enrichService.enrich(params,function (err, data) {
         if(err){handleError(err);}
         else{
-          var content = enrichmentToHtml(data);
+          var content = enrichmentToDonutHtml(data);
           layer.bindPopup(content).openPopup();
         }
       });
 
     }
 }
-function enrichmentToHtml(res){
-  var htmlContent = [];
-  var attributes = res.results[0].value.FeatureSet[0].features[0].attributes;
-  $.each(res.results[0].value.FeatureSet[0].fields,function(i,field){
-    htmlContent.push('<p>',field.alias,':',attributes[field.name],'</p>');
-  });
-  return htmlContent.join('');
-}
 
-/*function enrichmentToDonutHtml(res){
-  var htmlContent = [];
-  var attributes = res.results[0].value.FeatureSet[0].features[0].attributes;
-  $.each(res.results[0].value.FeatureSet[0].fields,function(i,field){
-    htmlContent.push('<p>',field.alias,':',attributes[field.name],'</p>');
-  });
-  return htmlContent.join('');
-}*/
-
+//Generic AGO error handler
 function handleError(err){
   if(err && err.code && err.code == 498){
     alert('Oh no! Your arcgis.com token expired. Dismiss this message to log in again.');
@@ -97,3 +82,163 @@ $('#addPinBtn').on('click', function (e) {
     L.marker(e.latlng).addTo(map);
   });
 });
+
+
+//////////////d3 related
+
+/**
+ * Returns a detached div containing SVG based legend and donut charts
+ * for the proved json data. The data param must be an AGO GeoEnrichment
+ * JSON response.  Method depends on d3 and bootstrap css.
+ * @TODO method is currently specific to AGE variables, but should be made
+ * generic.
+ * @param data
+ * @returns {*}
+ */
+function enrichmentToDonutHtml(data){
+  var detachedHtml = d3.select(document.createElement("div"))
+    .attr("class","row");
+  var legendHtml = detachedHtml.append("div")
+    .attr("class","col-md-4");
+  var chartHtml = detachedHtml.append("div")
+      .attr("class","col-md-8");
+
+  var radius = 74,
+    padding = 10;
+  var color = d3.scale.category20();
+
+  var arc = d3.svg.arc()
+    .outerRadius(radius)
+    .innerRadius(radius - 40);
+
+  var pie = d3.layout.pie()
+    .sort(null)
+    .value(function (d) {
+      return d.value;
+    });
+
+    var summaryFieldFunctions = [
+      {field: 'total', func: function (entries) {
+        return d3.sum(entries, function (entry) {
+          return (entry.key.indexOf('MALE') > -1 || entry.key.indexOf('FEM') > -1) ? entry.value : 0;
+        })
+      }},
+      {field: 'maleTotal', func: function (entries) {
+        return d3.sum(entries, function (entry) {
+          return (entry.key.indexOf('MALE') > -1) ? entry.value : 0;
+        })
+      }},
+      {field: 'femaleTotal', func: function (entries) {
+        return d3.sum(entries, function (entry) {
+          return (entry.key.indexOf('FEM') > -1) ? entry.value : 0;
+        })
+      }}
+    ];
+
+    data = mergeFieldsAndValues(data, summaryFieldFunctions);
+
+    var dataSets = [];
+    dataSets.push(data.filter(function (d) {return d.name.indexOf('MALE') > -1;}));
+    dataSets.push(data.filter(function (d) {return d.name.indexOf('FEM') > -1;}));
+
+    color.domain(dataSets[0].map(function (d) {return d.alias.split(' ')[2]}));
+
+    var legend = legendHtml.append("svg")
+      .attr("class", "legend")
+      .attr("width", radius)
+      .attr("height", radius * 5)
+      .selectAll("g")
+      .data(color.domain().slice().reverse())
+      .enter().append("g")
+      .attr("transform", function (d, i) {
+        return "translate(0," + i * 20 + ")";
+      });
+
+    legend.append("rect")
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", color);
+
+    legend.append("text")
+      .attr("x", 24)
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .text(function (d) {
+        return d;
+      });
+
+
+    var numFormat = d3.format(",");
+    var perFormat = d3.format("%");
+
+    // console.log([values]);
+    var svg = chartHtml.selectAll(".pie")
+      .data(dataSets)
+      .enter().append("svg")
+      .attr("class", "pie")
+      .attr("width", radius * 2)
+      .attr("height", radius * 2)
+      .append("g")
+      .attr("transform", "translate(" + radius  + "," + radius + ")");
+
+    svg.selectAll(".arc")
+      .data(function (d, i) {
+        return pie(dataSets[i])
+      })
+      .enter().append("path")
+      .attr("class", "arc")
+      .attr("d", arc)
+      .style("fill", function (d) {
+        return color(d.data.alias.split(' ')[2]);
+      })
+      .append("title")
+      .text(function (d, i) {
+        var isMale = d.data.name.indexOf('MALE') > -1;
+        return [d.data.alias, ': ', numFormat(d.data.value), '\n',
+          perFormat(d.data.value / d.data.total), ' of total population', '\n',
+          perFormat(d.data.value / d.data[isMale ? 'maleTotal' : 'femaleTotal']),
+          'of ', isMale ? 'male ' : 'female ', 'population'].join(' ');
+      });
+
+
+    svg.append("text")
+      .attr("dy", ".35em")
+      .style("text-anchor", "middle")
+      .text(function (d) {
+        return d[0].alias.split(',')[0];
+      });
+
+    return detachedHtml[0][0];
+}
+
+/**
+ * Given a geoenrichment result, merge values with corresponding fields.
+ * If optional summaryFunctions param is provided iterate summary functions
+ * with entries to produce summary vars (e.g. sum, max, mean, etc.).  The
+ * dynamic summary fields will be appended to each field object.
+ *
+ * SummaryFunctions takes the form:
+ * [{field:'Your Field Name',func:function(entries){...return your summation here},...]
+*
+* @param data
+ * @param summaryFunctions
+ * @returns {*}
+ */
+function mergeFieldsAndValues(data, summaryFunctions) {
+  var fields = data.results[0].value.FeatureSet[0].fields;
+  var values = data.results[0].value.FeatureSet[0].features[0].attributes;
+  var entries = d3.entries(values);
+  var summaries = {};
+  if (summaryFunctions) {
+    for (var i = 0; i < summaryFunctions.length; i++) {
+      summaries[summaryFunctions[i].field] = summaryFunctions[i].func(entries);
+    }
+  }
+  for (var i = 0; i < fields.length; i++) {
+    fields[i].value = values[fields[i].name];
+    for (var key in summaries) {
+      fields[i][key] = summaries[key];
+    }
+  }
+  return fields;
+}
